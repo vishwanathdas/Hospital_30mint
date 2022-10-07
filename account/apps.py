9695d84e9bfc6d0b080cebea65bb893b2adad893
Vishwanath Das <111034515+vishwanathdas@users.noreply.github.com>
2022-10-07 14:02:47 +0530

    hospital

---
+++ account/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+from django.apps import AppConfig
+
+class ExampleConfig(AppConfig):
+    default_auto_field = 'django.db.models.AutoField'
+
+
+class AccountConfig(AppConfig):
+    name = 'account'
---
+++ account/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render, redirect
+from .forms import SignUpForm, LoginForm
+from django.contrib.auth import authenticate, login
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def register(request):
+    msg = None
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            msg = 'user created'
+            return redirect('login_view')
+        else:
+            msg = 'form is not valid'
+    else:
+        form = SignUpForm()
+    return render(request,'register.html', {'form': form, 'msg': msg})
+
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+    msg = None
+    if request.method == 'POST':
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None and user.is_admin:
+                login(request, user)
+                return redirect('adminpage')
+            elif user is not None and user.is_patient:
+                login(request, user)
+                return redirect('patient')
+            elif user is not None and user.is_doctor:
+                login(request, user)
+                return redirect('doctor')
+            else:
+                msg= 'invalid credentials'
+        else:
+            msg = 'error validating form'
+    return render(request, 'login.html', {'form': form, 'msg': msg})
+
+
+def admin(request):
+    return render(request,'admin.html')
+
+
+def patient(request):
+    return render(request,'patient.html')
+
+
+def doctor(request):
+    return render(request,'doctor.html')
